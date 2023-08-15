0b9132a8d4e5baf9511b535dfb3976674bdd5c5d
James Newton <blackslate@lexogram.com>
2023-08-15 07:52:28 +0300

    Rotating cube; stops when clicked, rollover hilite
    * Remove boilerplate from App.tsx, App.css and index.css
    * Add CSS width!important and height!important for canvas
    * Import Canvas and useFrame from @react-three/fiber
    * Create Box component with animation and interaction
    * Wrap Box in a Group
    * Add Canvas with ambientLight, pointLight and Group

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+type Rotation = {
+  x: number
+}
+type BoxMesh = {
+  rotation: Rotation
+} | undefined
+type BoxRef = any // sledgehammer to keep TypeScript quiet
+
+
+
+function Box(props:object) {
+  const ref:BoxRef = useRef()  
+
+  const [hovered, hover] = useState(false)
+  // const [ clicked, click ] = useState(false)
+  const [ active, setActive ] = useState(true)
+
+  useFrame((_state, delta) => {
+    const boxMesh:BoxMesh = ref.current
+    
+    if (boxMesh && boxMesh["rotation"] && active) {
+      const rotation:Rotation = boxMesh["rotation"]
+      rotation["x"] += delta
+    }
+  })
 
   return (
+    <mesh
+      {...props}
+      ref={ref}
+      // scale={clicked ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry
+        args={[1,1,1]}
+      />
+      <meshStandardMaterial
+        color={hovered ? "orange" : "#888"}
+      />
+    </mesh>
+  )
+}
+
+
+function Group() {
+  return (
+    <group
+      position={[1,2,-5]}
+    >
+      <Box />
+    </group>
+  )
+}
+
+
+function App() {
+  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Canvas>
+        <ambientLight />
+        <pointLight
+          position={[1,4,9]}
+          intensity={256}
+          color={"#ff0"}
+        />
+        <Group />
+      </Canvas>
     </>
   )
 }
